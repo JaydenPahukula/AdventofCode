58c3a1f5b78da34819cb665d6de1198bd549b3c4
Jayden Pahukula <72293021+JaydenPahukula@users.noreply.github.com>
2022-12-04 22:27:41 -0700

    day 5

---
+++ day5/part1.py
@@ -0,0 +1,23 @@
+
+def solution():
+
+    with open("day5/input.txt", "r") as file:
+        data = [x.replace('\n','') for x in file.readlines() if x != ""]
+    
+    stacks = ["RPCDBG", "HVG", "NSQDJPM", "PSLGDCNM", "JBNCPFLS", "QBDZVGTS", "BZMHFTQ", "CMDBF", "FCQG"]
+    
+    for line in data:
+        instruction = line.split(' ')
+        
+        for i in range(int(instruction[1])):
+            stacks[int(instruction[5])-1] += stacks[int(instruction[3])-1][-1]
+            stacks[int(instruction[3])-1] = stacks[int(instruction[3])-1][:-1]
+
+    final = ""
+    for stack in stacks:
+        final += stack[-1]
+
+    return final
+
+
+print(solution())
---
+++ day5/part2.py
@@ -0,0 +1,22 @@
+
+def solution():
+
+    with open("day5/input.txt", "r") as file:
+        data = [x.replace('\n','') for x in file.readlines() if x != ""]
+    
+    stacks = ["RPCDBG", "HVG", "NSQDJPM", "PSLGDCNM", "JBNCPFLS", "QBDZVGTS", "BZMHFTQ", "CMDBF", "FCQG"]
+
+    for line in data:
+        instruction = line.split(' ')
+        
+        stacks[int(instruction[5])-1] += stacks[int(instruction[3])-1][-int(instruction[1]):]
+        stacks[int(instruction[3])-1] = stacks[int(instruction[3])-1][:-int(instruction[1])]
+
+    final = ""
+    for stack in stacks:
+        final += stack[-1]
+
+    return final
+
+
+print(solution())
