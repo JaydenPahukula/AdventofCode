f0d0863ddfa0ecadf4f67fcbd7e0cc3b3d81cac6
Jayden Pahukula <72293021+JaydenPahukula@users.noreply.github.com>
2022-12-06 00:18:16 -0700

    day 6

---
+++ day6/part2.py
@@ -0,0 +1,18 @@
+
+def solution():
+
+    with open("day6/input.txt", "r") as file:
+        data = [x.replace('\n','') for x in file.readlines() if x != ""][0]
+    
+    x = list(data[:13])
+    i = 14
+    for c in data[13:]:
+        x.append(c)
+        print(x)
+        if len(x) == len(set(x)):
+            return i
+        x.pop(0)
+        i += 1
+
+
+print(solution())
